f5c9a9f6d8c626b50fe7c7afeb14139e2ba903c5
joseignaciorc <riano@joseignac.io>
2021-12-03 22:42:49 +0100

    [TEST] Test installation of GE inside a zip file (PEP 273)
    This commit adds a Github Action that checks automatically if issue 3772 ('NotADirectoryError occurs when GE is installed in a zip file and build_data_docs() is called') happens.
    The issue itself will be fixed in a another commit

---
+++ tests/integration/common_workflows/simple_build_data_docs.py
@@ -0,0 +1,21 @@
+import os
+import tempfile
+
+import great_expectations as ge
+from great_expectations.data_context import BaseDataContext
+from great_expectations.data_context.types.base import (
+    DataContextConfig,
+    DatasourceConfig,
+    FilesystemStoreBackendDefaults,
+)
+
+print(f"Great Expectations version: {ge.__version__}")
+data_context_config = DataContextConfig(
+    datasources={"example_datasource": DatasourceConfig(class_name="PandasDatasource")},
+    store_backend_defaults=FilesystemStoreBackendDefaults(
+        root_directory=tempfile.mkdtemp() + os.sep + "my_greatexp_workdir"
+    ),
+)
+
+context = BaseDataContext(project_config=data_context_config)
+print(context.build_data_docs())
